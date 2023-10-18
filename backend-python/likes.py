b1181ff7facc78f2c77d082b922158658f927583
Brendanmpeg <112417458+Brendanmpeg@users.noreply.github.com>
2023-10-18 18:01:03 -0400

    Brendans inital push

---
+++ backend-python/likes.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template_string, render_template, request, Response, make_response, redirect, url_for
+from pymongo import MongoClient
+import bcrypt, random
+from db import *
+
+app = Flask(__name__)
+
+mongo_client = MongoClient('localhost', 27017)
+
+
+def validate_user(auth_token):
+    exists, user = dbExist("feature","sessionToken", auth_token) # equivalent database.find_one(auth_token) #returns None if the user does not exist
+    if exists:
+        username = user["username"]
+        return username
+    else:
+        return None
+
+def like(self, post_id):
+    post = dbQuery("_id", post_id)
+
