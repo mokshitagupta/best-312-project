6d3710ce0eb4363a96c2661583f6e0178ea8c1d2
Brendanmpeg <112417458+Brendanmpeg@users.noreply.github.com>
2023-10-19 13:38:45 -0400

    likes are updated

--- backend-python/app.py
+++ backend-python/app.py
@@ -4,17 +4,16 @@ import bcrypt, random
 from db import *
 from likes import *
 
+
 class ConfigClass(object):
     # Flask settings
     SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
 
 
-
 def create_app():
-    
     # Setup Flask and load app.config
     app = Flask(__name__)
-    app.config.from_object(__name__+'.ConfigClass')
+    app.config.from_object(__name__ + '.ConfigClass')
 
     # The Home page is accessible to anyone
     @app.route('/')
@@ -25,26 +24,26 @@ def create_app():
                 <p><a href="/register">Register</a></p>
                 <p><a href="/login">Login</a></p>
             """
-    
-    @app.route('/register', methods=['POST',"GET"])
+
+    @app.route('/register', methods=['POST', "GET"])
     def register():
-        if request.method == "POST" :
+        if request.method == "POST":
             form = request.form
             username = form["username"]
             password = form["password"]
 
             exists = dbQuery("username", username, all=False, raw=True) != []
             if exists:
-                return render_template('register.html', feedback="user already in db"),401
-            
+                return render_template('register.html', feedback="user already in db"), 401
+
             s = getSalt()
 
             userEntry = {
-                "_id" : increment(),
-                "path" : "registeredUsers",
+                "_id": increment(),
+                "path": "registeredUsers",
                 "username": username,
-                "password" : bcrypt.hashpw(password.encode(), s),
-                "xsrf": "".join([str(random.randint(0,9) )for _ in range(5)])
+                "password": bcrypt.hashpw(password.encode(), s),
+                "xsrf": "".join([str(random.randint(0, 9)) for _ in range(5)])
             }
 
             print(userEntry, " <----- inserted to db")
@@ -53,13 +52,13 @@ def create_app():
 
             # return render_template('register.html', feedback="Account succesfully created"),200        
         else:
-        # String-based templates
-            
+            # String-based templates
+
             return render_template('register.html')
-    
-    @app.route('/login', methods=['POST',"GET"])
+
+    @app.route('/login', methods=['POST', "GET"])
     def login():
-        if request.method == "POST" :
+        if request.method == "POST":
             form = request.form
             username = form["username"]
             password = form["password"]
@@ -69,35 +68,34 @@ def create_app():
             exists = entry != []
 
             if not exists:
-                return render_template('login.html', feedback="user not in db"),404
+                return render_template('login.html', feedback="user not in db"), 404
 
-            #verify passwords
+            # verify passwords
             verif = bcrypt.checkpw(password.encode("utf-8"), entry["password"])
             # #print("ABLE TO VERIFY PASSWORD !!!")
             if not verif:
-                return render_template('login.html', feedback="invalid credentials"),403
+                return render_template('login.html', feedback="invalid credentials"), 403
 
-            token  = "".join([str(random.randint(0,9) )for _ in range(10)])
+            token = "".join([str(random.randint(0, 9)) for _ in range(10)])
             s = getSalt()
             hash = bcrypt.hashpw(token.encode(), s)
 
             insertSessionId(hash, username)
 
             # resp = make_response(render_template('profile.html', username=username))
-            
-            response = make_response(redirect(url_for("profilePage",id=str(entry["_id"]))))
-            response.set_cookie("token", value = token, max_age = 60 * 60 * 24, httponly = True)
+
+            response = make_response(redirect(url_for("profilePage", id=str(entry["_id"]))))
+            response.set_cookie("token", value=token, max_age=60 * 60 * 24, httponly=True)
 
             return response
-        else :
+        else:
             return render_template('login.html')
-
 
     @app.route('/profile/<id>')
     def profilePage(id):
         # String-based templates
-        entry =  dbQuery("_id", int(id), all=False, raw=True)
-        print(entry,id)
+        entry = dbQuery("_id", int(id), all=False, raw=True)
+        print(entry, id)
         return render_template('profile.html', username=entry["username"], username_hidden=entry["username"])
 
     @app.route('/add-post', methods=["POST"])
@@ -109,18 +107,56 @@ def create_app():
         name = request.cookies.get('token')
 
         entry = {
-            "_id" : increment(),
-            "title":title,
-            "detail" : detail,
-            "username" : username,
-            "feature":"posts"
+            "_id": increment(),
+            "title": title,
+            "detail": detail,
+            "username": username,
+            "feature": "posts"
         }
-        
 
         dbInsert(entry)
         comments = dbQuery("feature", "posts", all=True, raw=True)
 
         return redirect(request.referrer)
+
+    @app.route('/add-post', methods=["POST"])
+    def addPost():
+        form = request.form
+        title = form["title"]
+        detail = form["detail"]
+        username = form["username"]
+        name = request.cookies.get('token')
+        likes = []
+
+        entry = {
+            "_id": increment(),
+            "title": title,
+            "detail": detail,
+            "username": username,
+            "feature": "posts",
+            "likes": likes
+        }
+
+        # If the user is authenticated post the message
+        if validate_user(name) != None:
+            dbInsert(entry)
+            comments = dbQuery("feature", "posts", all=True, raw=True)
+
+            return redirect(request.referrer)
+
+        # else return to index.html since user is not authenticated
+        else:
+            return render_template('index.html', feedback="user is not authenticated"), 401
+
+    @app.route('/like', methods=["POST"])
+    def like_post():
+        auth_token = request.cookies.get('token')
+        username = validate_user(auth_token)
+        if username is not None:
+            post_id = ""
+            like(post_id, username)
+            pass
+        return
 
     @app.route('/get-posts', methods=["GET"])
     def getPosts():
@@ -132,6 +168,6 @@ def create_app():
 
 
 # Start development web server
-if __name__=='__main__':
+if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=8080, debug=True)
--- backend-python/likes.py
+++ backend-python/likes.py
@@ -9,14 +9,16 @@ mongo_client = MongoClient('localhost', 27017)
 
 
 def validate_user(auth_token):
-    exists, user = dbExist("feature","sessionToken", auth_token) # equivalent database.find_one(auth_token) #returns None if the user does not exist
+    exists, user = dbExist("feature", "sessionToken", auth_token)
+    # equivalent database.find_one(auth_token) #returns None if the user does not exist
     if exists:
         username = user["username"]
         return username
     else:
         return None
 
-def like(self, post_id, username):
+
+def like(post_id, username):
     post = dbQuery("_id", post_id)
     likes = set(post["likes"])
     if username in likes:
