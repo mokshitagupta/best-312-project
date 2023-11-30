45a4d35e17cbf8bf92a714d0006118e5b705ffff
Tina Costanza <accostan@buffalo.edu>
2023-11-30 14:35:41 -0500

    MAYBE?

--- backend-python/static/comments.js
+++ backend-python/static/comments.js
@@ -3,7 +3,7 @@ let socket = null
 
 function websocketConn(id){
     console.log("loaded", id)
-    socket = io('https://localhost:8080', {
+    socket = io('https://localhost:443', {
         transports: ['websocket'], 
         upgrade: false
     });
