6c65c01f902cf85d363eeda29f743ece463d1925
Tina Costanza <accostan@buffalo.edu>
2023-12-01 09:22:25 -0500

    MAYBE?

--- backend-python/static/comments.js
+++ backend-python/static/comments.js
@@ -3,7 +3,7 @@ let socket = null
 
 function websocketConn(id){
     console.log("loaded", id)
-    socket = io('https://bidbig.live', {
+    socket = io.connect('https://bidbig.live', {
         transports: ['websocket'],
         upgrade: true
     });
