91bb1018af13d94741d7b31a3622c301798b4841
CJFran10 <cj.fran02@hotmail.com>
2023-12-01 22:36:32 -0500

    Testing 10 per hour

--- backend-python/app.py
+++ backend-python/app.py
@@ -27,7 +27,7 @@ def create_app():
     limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["50/10seconds"],
+    default_limits=["10 per hour"],
     storage_uri="memory://",
 )
 
@@ -341,7 +341,7 @@ def create_app():
         return resplog
 
     @app.before_request
-    @limiter.limit("50/10seconds")
+    @limiter.limit("10 per hour")
     def attack():
         return "Too many Requests"
 
